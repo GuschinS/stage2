f270c5f7afe251a370b9e1f85dc2cd558efe8f07
guschins <guschins@gmail.com>
2022-10-20 15:41:51 +0300

    feat: add change position by click

--- gem-puzzle/src/js/game-field.js
+++ gem-puzzle/src/js/game-field.js
@@ -1,8 +1,11 @@
 import { h1, buttonStart, controlContainer } from "./control";
+import click from "../assets/sound/click.mp3";
+import error from "../assets/sound/error.mp3";
 
 const body = document.querySelector("body");
 const main = document.createElement("main");
 const container = document.createElement("div");
+const volume = controlContainer.querySelector(".volume");
 const values = new Array(16).fill(0).map((item, index) => {
   index;
 });
@@ -29,6 +32,32 @@ buttonStart.addEventListener("click", () => {
   score = 0;
   scoreDiv.textContent = `Score: ${score}`;
   container.classList.add("play");
+});
+
+const blankNumber = 16;
+container.addEventListener("click", (event) => {
+  const buttonNode = event.target.closest("button");
+  const buttonNumber = Number(buttonNode.dataset.matrixId);
+  const buttonCoords = findCoordinatesByNumber(buttonNumber, matrix);
+  const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
+  const isValid = isValidForSwap(buttonCoords, blankCoords);
+  const scoreDiv = document.querySelector(".score");
+
+  if (isValid) {
+    swap(blankCoords, buttonCoords, matrix);
+    setPositionItems(matrix);
+    score += 1;
+    scoreDiv.textContent = `Score: ${score}`;
+    const clickItem = new Audio(click);
+    if (volume.classList.value != "volume mute") {
+      clickItem.play();
+    }
+  } else {
+    if (volume.classList.value != "volume mute") {
+      const clickItem = new Audio(error);
+      clickItem.play();
+    }
+  }
 });
 
 //Functions
@@ -84,4 +113,30 @@ function shuffleArray(arr) {
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
+}
+
+function findCoordinatesByNumber(number, matrix) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] == number) {
+        return { x, y };
+      }
+    }
+  }
+  return null;
+}
+
+function isValidForSwap(coords1, coords2) {
+  const diffX = Math.abs(coords1.x - coords2.x);
+  const diffY = Math.abs(coords1.y - coords2.y);
+  return (
+    (diffX === 1 || diffY === 1) &&
+    (coords1.x === coords2.x || coords1.y === coords2.y)
+  );
+}
+
+function swap(coords1, coords2, matrix) {
+  const coords1Number = matrix[coords1.y][coords1.x];
+  matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x];
+  matrix[coords2.y][coords2.x] = coords1Number;
 }
