49110bf51f7e48ed2d3fe70da3b4121d799bb33f
guschins <guschins@gmail.com>
2022-10-20 15:25:51 +0300

    refactor: change game field

--- gem-puzzle/src/js/game-field.js
+++ gem-puzzle/src/js/game-field.js
@@ -1,25 +1,62 @@
 const body = document.querySelector("body");
-const createGameField = () => {
-  const table = document.createElement("table");
-  const tbody = document.createElement("tbody");
-  //   const tr = document.createElement("tr");
+const main = document.createElement("main");
+const container = document.createElement("div");
+const values = new Array(16).fill(0).map((item, index) => {
+  index;
+});
 
-  let columnCount = 4;
-  let rowCount = 4;
-
-  for (let index = 0; index < rowCount; index++) {
-    const tr = document.createElement("tr");
-    tbody.append(tr);
-    console.log("tr: ", tr);
-    for (let index = 0; index < columnCount; index++) {
-      const td = document.createElement("td");
-      tr.append(td);
-      console.log("td: ", td);
-    }
-  }
-
-  table.append(tbody);
-  body.append(table);
-};
+//Create
 
 createGameField();
+
+//Position
+
+const items = Array.from(container.querySelectorAll(".item"));
+let matrix = getMatrix(items.map((item) => item.dataset.matrixId));
+
+setPositionItems(matrix);
+
+//Functions
+
+function createGameField() {
+  container.classList = "container";
+  for (let value in values) {
+    const button = document.createElement("button");
+    button.classList = "item";
+    button.dataset.matrixId = Number(value) + 1;
+    button.innerHTML = Number(value) + 1;
+    container.append(button);
+  }
+  main.append(container);
+  body.append(main);
+}
+
+function getMatrix(arr) {
+  const matrix = [[], [], [], []];
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (x >= 4) {
+      y++;
+      x = 0;
+    }
+    matrix[y][x] = arr[i];
+    x++;
+  }
+  return matrix;
+}
+
+function setPositionItems(matrix) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      const value = matrix[y][x];
+      const node = items[value - 1];
+      setNodeStyles(node, x, y);
+    }
+  }
+}
+
+function setNodeStyles(node, x, y) {
+  const shiftPs = 100;
+  node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%, 0)`;
+}
