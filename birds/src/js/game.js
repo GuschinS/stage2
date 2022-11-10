6644b323ae0769b3e969c0ed11647307c2731d9d
guschins <guschins@gmail.com>
2022-11-10 15:21:13 +0300

    feat: add static elements

---
+++ birds/src/js/game.js
@@ -0,0 +1,8 @@
+import birdsData from "./birds";
+
+const birdImage = document.querySelector(".bird-image");
+// console.log("imageBird: ", imageBird);
+// if (birdImage) {
+//   birdImage.src = `${birdsData[0][0].image}`;
+// }
+// console.log(birdsData[0][0].image);
