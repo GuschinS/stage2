08790ae4a557a491ac06c8e9a722c46dafa2a9b7
guschins <guschins@gmail.com>
2022-11-16 16:11:18 +0300

    feat: add volume control

--- birds/src/js/audio.js
+++ birds/src/js/audio.js
@@ -85,4 +85,22 @@ if (play) {
     setInterval(updateProgressValue, 500);
   });
 }
+
+//Sound
+
+const volumeIcon = document.querySelector(".volume-icon");
+const soundVolume = document.getElementById("sound-volume");
+
+const toggleVolume = () => {
+  volumeIcon.classList.toggle("icono-volumeMute");
+  volumeIcon.classList.toggle("icono-volumeHigh");
+  audio.muted === false ? (audio.muted = true) : (audio.muted = false);
+};
+const soundVolumeControl = () => {
+  audio.volume = soundVolume.value;
+};
+
+volumeIcon.addEventListener("click", toggleVolume);
+soundVolume.addEventListener("input", soundVolumeControl);
+
 export { index, resetProgressBar };
