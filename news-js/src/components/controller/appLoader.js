bb297558fde994432f0abb008a5d90db517c568e
guschins <guschins@gmail.com>
2022-12-08 00:00:38 +0300

    feat: add api-key

--- news-js/src/components/controller/appLoader.js
+++ news-js/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2335fb0913a44c03a20a84ca0c95b280', // получите свой ключ https://newsapi.org/
         });
     }
 }
