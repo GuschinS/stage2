b5f37803c2ddc7e67133c5b4722f1b2239410563
guschins <guschins@gmail.com>
2022-12-07 20:57:19 +0300

    feat: add typescript

--- news-js/webpack.config.js
+++ news-js/webpack.config.js
@@ -1,25 +1,63 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
+// const path = require('path');
+// const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+// const baseConfig = {
+//     entry: path.resolve(__dirname, './src/index.js'),
+//     mode: 'development',
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/i,
+//                 use: ['style-loader', 'css-loader'],
+//             },
+//         ],
+//     },
+//     resolve: {
+//         extensions: ['.js'],
+//     },
+//     output: {
+//         filename: 'index.js',
+//         path: path.resolve(__dirname, '../dist'),
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             template: path.resolve(__dirname, './src/index.html'),
+//             filename: 'index.html',
+//         }),
+//         new CleanWebpackPlugin(),
+//     ],
+// };
+
+// module.exports = ({ mode }) => {
+//     const isProductionMode = mode === 'prod';
+//     const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+//     return merge(baseConfig, envConfig);
+// };
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
     mode: 'development',
+
+    // devtool: 'inline-source-map',
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -28,11 +66,4 @@ const baseConfig = {
         }),
         new CleanWebpackPlugin(),
     ],
-};
-
-module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
-
-    return merge(baseConfig, envConfig);
 };
